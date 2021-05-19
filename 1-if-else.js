09b6add77749821a0dc4e9132511855eccd81369
Dipesh Viradiya <dipesh40939@gmail.com>
2021-05-19 12:06:54 +0530

    Basic demo test If Else to ternary

---
+++ 1-if-else.js
@@ -0,0 +1,26 @@
+console.log("Hello , World...")
+
+let message;
+let login = '';
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+/*Rewrite the code*/
+
+login == 'Employee' ?
+  message = 'Hello':
+    login == 'Director' ?
+  message = 'Greetings':
+    login == ''?
+  message = 'No login':
+  message = '';
+
+
+console.log(message)
