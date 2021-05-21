89648273a6898a1a6e0b0c4ebae2cf59b5a0b6c3
Dipesh Viradiya <dipesh40939@gmail.com>
2021-05-21 10:43:47 +0530

    Initial commit

---
+++ 8-Promises.js
@@ -0,0 +1,21 @@
+/*
+The built-in function setTimeout uses callbacks. Create a promise-based alternative.
+
+The function delay(ms) should return a promise. That promise should resolve after ms milliseconds, so that we can add .then to it, like this:
+
+function delay(ms) {
+  // your code
+}
+
+delay(3000).then(() => alert('runs after 3 seconds'));
+*/
+
+function delay(ms) {
+  // your code
+    let promise = new Promise((resolve, reject)=>{
+        setTimeout(resolve, ms);
+    });
+    return promise;
+}
+
+delay(3000).then(() => alert('runs after 3 seconds'));
