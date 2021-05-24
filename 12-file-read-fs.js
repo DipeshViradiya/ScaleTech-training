2bcadfd7a691a4a9d96b4a890d108149af7b1b4e
Dipesh Viradiya <dipesh40939@gmail.com>
2021-05-24 18:54:48 +0530

    Tree problem solved...

--- 12-file-read-fs.js
+++ 12-file-read-fs.js
@@ -10,3 +10,24 @@ fs.readFile('demo.txt', function (err, data) {
 
 var data = fs.readFileSync('demo.txt');
 console.log("Sync data : " + data.toString());
+
+/*
+
+O/P :
+
+PS D:\ScaleTech\Training JS> node .\12-file-read-fs.js
+Sync data : This is demo file.
+It will be read by node program.
+
+
+- Dipesh Viradiya
+:)
+File data : This is demo file.
+It will be read by node program.
+
+
+- Dipesh Viradiya
+:)
+PS D:\ScaleTech\Training JS>
+
+ */
--- Week 2/tree1.js
+++ Week 2/tree1.js
@@ -54,37 +54,92 @@ class BinaryTree {
         }
 
         return 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right));
-    };
+    }
+
+    findAndPrintHierarchy(root, k) {
+        if(root === null) {
+            return;
+        }
+        stack.push(root.data);
+        this.findAndPrintHierarchy(root.left, k);
+        this.findAndPrintHierarchy(root.right, k);
+        
+        let sum = 0;
+
+        for(let i = stack.length - 1; i >= 0 ; i--) {
+            sum = sum + stack[i];
+            if(sum === k) {
+                for(let j = i; j< stack.length ; j++) {
+                    console.log(stack[j] + " ");
+                }
+                console.log();
+            }
+        }
+        stack.pop();
+    }
 }
 var binaryTreeObj = new BinaryTree();
-var inputDepth = 2;
+let stack = [];
+
+/*
+
+---------- This code is to generate random integers from 0 to 99 in tree----------
+
+var inputDepth = 5;
 do{
     binaryTreeObj.insertData(Math.floor(Math.random() * 100));
 } while (binaryTreeObj.maxDepth(binaryTreeObj.root) <= inputDepth)
 console.log(binaryTreeObj.inorder(binaryTreeObj.root));
 
-
-/*
-binaryTreeObj.insertData(7);
-binaryTreeObj.insertData(2);
-binaryTreeObj.insertData(5);
-binaryTreeObj.insertData(1);
-binaryTreeObj.insertData(3);
-binaryTreeObj.insertData(7);
-binaryTreeObj.insertData(4);
-binaryTreeObj.insertData(6);
-binaryTreeObj.insertData(45);
-console.log(binaryTreeObj.root + " " + binaryTreeObj.node);
-console.log(Object.keys(binaryTreeObj));
-console.log(Object.values(binaryTreeObj));
-console.log(binaryTreeObj.maxDepth(binaryTreeObj.root));
-console.log(binaryTreeObj.inorder(binaryTreeObj.root));
 */
 
+/*
+---the test code for small tree---*/
+binaryTreeObj.insertData(4);
+binaryTreeObj.insertData(2);
+binaryTreeObj.insertData(6);
+binaryTreeObj.insertData(1);
+binaryTreeObj.insertData(3);
+binaryTreeObj.insertData(5);
+binaryTreeObj.insertData(7);
 
 
+//binaryTreeObj.insertData(8);
+//binaryTreeObj.insertData(45);
+//console.log(binaryTreeObj.root + " " + binaryTreeObj.node);
+//console.log(Object.keys(binaryTreeObj));
+//console.log(Object.values(binaryTreeObj));
+//console.log(binaryTreeObj.maxDepth(binaryTreeObj.root));
+//console.log(binaryTreeObj.inorder(binaryTreeObj.root));
+binaryTreeObj.findAndPrintHierarchy(binaryTreeObj.root, 6);
 
 
+/*
+
+         4
+         
+  2             6
+1    3      5      7
 
 
+*/
 
+/*
+OUTPUT:
+PS D:\ScaleTech\Training JS\Week 2> node .\tree1.js
+new root added : [object Object]4
+left node added[object Object]2  
+right node added[object Object]6 
+left node added[object Object]1  
+right node added[object Object]3 
+left node added[object Object]5  
+right node added[object Object]7 
+4 
+2 
+
+6 
+
+DONE
+
+ */
+console.log("DONE");
