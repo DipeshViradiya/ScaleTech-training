931007309e5d5017916ae7b8081a5e4d77ed9e34
Dipesh Viradiya <dipesh40939@gmail.com>
2021-05-27 18:45:53 +0530

    CRUD operations applied...

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -4,6 +4,8 @@ var hbs = require('hbs');
 var path = require('path');
 var db = require('./db');
 var viewPath = path.join(__dirname, './templates/view');
+
+app.use(express.urlencoded({extended : false}));
 app.set('view engine', 'hbs');
 app.set('views',viewPath);
 
@@ -18,7 +20,53 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/create-task', (req, res) => {
+    res.render('create-task');
+})
 
+app.post('/create-task', function (req, res){
+    var title = req.body.title;
+    var desc = req.body.desc;
+
+    var sql = `insert into tasks(title, detail, created, isactive) values ("${title}", "${desc}", now(), 0)`;
+    db.query(sql, function(err, result){
+        if(err) throw err;
+        console.log("record inserted");
+        res.redirect('/');
+    })
+})
+
+app.get('/edit-task/:id', (req, res) => {
+    var id = req.params.id;
+    var sql = `select * from tasks where id= ${id}`;
+    db.query(sql, function(err, rows, fields) {
+        res.render('edit-task', {task : rows[0]});
+    })
+})
+
+app.post('edit-task/:id', (req, res) => {
+    var title = req.body.title;
+    var detail = req.body.desc;
+    var id = req.params.id;
+    var sql = `update tasks set title = ${title} , detail = ${desc} where id = ${id}`;
+    db.query(sql, function(err, result) {
+        if(err) throw err;
+        console.log("record updated...");
+        res.redirect('/');
+    })
+    
+})
+
+app.get('/delete-task/:id', (req, res) => {
+    var id = req.params.id;
+    var sql = `delete from tasks where id = ${id}`
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log("record updated...");
+        res.redirect('/');
+    })
+
+})
 
 app.listen(7000, (err) => {
     if(err){
